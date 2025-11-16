0bcd0664626160725b25938d87869c5409c5f410
abdlkader <abdlkadirdarwih@gmail.com>
2025-11-16 13:33:42 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ const app = express()
 
 dotenv.config();
 app.use(express.json())
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 // app.use(cors())
 // app.use(cors({
 //   origin: "https://school-f.vercel.app",
@@ -200,6 +202,8 @@ if (!fs.existsSync(uploadDir)) {
 //     res.status(500).json({ error: err.message });
 //   }
 // });
+
+
 app.post("/createEventBase64", async (req, res) => {
   try {
     const { date, place, title, text, mainImage, images } = req.body;
