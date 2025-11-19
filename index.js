84fbe1445f595ed2bcf6eeda95e8cacb048c1f47
abdlkader <abdlkadirdarwih@gmail.com>
2025-11-19 15:42:08 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ const app = express()
 
 dotenv.config();
 app.use(express.json())
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+// app.use(express.json({ limit: "10mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 // app.use(cors())
 // app.use(cors({
 //   origin: "https://school-f.vercel.app",
